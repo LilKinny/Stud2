db7359de312968c037ddc813b84d7e255e6842e8
BananaYangTaken <88693858+BananaYangTaken@users.noreply.github.com>
2022-02-16 13:41:42 +0800

    Update Unlock Functions

---
+++ 2122/enc_temp_folder/52597e130e348e9049dbf6defb375/EquipmentManager.cpp
@@ -0,0 +1,335 @@
+#include "EquipmentManager.h"
+
+EquipmentManager::EquipmentManager()
+{
+	MinigameBuffs = 0;
+	LuckyCatUpgrade = 0;
+	MoneyPlantUpgrade = 0;
+	PrestigeLvl = 0;
+	Money = 5000;
+	InitEquipArray();
+}
+
+EquipmentManager::~EquipmentManager()
+{
+	DeleteEquipArray();
+}
+
+int EquipmentManager::UpgradePrestige(bool Upgrade)
+{
+	if (PrestigeLvl == 0)
+	{
+		if (Money >= 500)
+		{
+			if (Upgrade == true)
+			{
+				Money = 0;
+				++PrestigeLvl;
+				LuckyCatUpgrade = MoneyPlantUpgrade = 0;
+				DeleteEquipArray();
+				InitEquipArray();
+			}
+			return 1;
+		}
+	}
+	else if(PrestigeLvl == 1) 
+	{
+		if (Money >= 1500)
+		{
+			if (Upgrade == true)
+			{
+				Money = 0;
+				++PrestigeLvl;
+				LuckyCatUpgrade = MoneyPlantUpgrade = 0;
+				DeleteEquipArray();
+				InitEquipArray();
+			}
+			return 1;
+		}
+	}
+	else if (PrestigeLvl == 2)
+	{
+		if (Money >= 4500)
+		{
+			if (Upgrade == true)
+			{
+				Money = 0;
+				++PrestigeLvl;
+				LuckyCatUpgrade = MoneyPlantUpgrade = 0;
+				DeleteEquipArray();
+				InitEquipArray();
+			}
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void EquipmentManager::CalculateTotalIncome(void)
+{
+	int TotalIncome = 0;
+	int TempIncome;
+	for (int i = 0; i < PrestigeLvl * 6; ++i)
+	{
+		TempIncome = 0;
+		EquipArray[i]->CalculateIncomePerSecond();
+		TempIncome += EquipArray[i]->IncomePerSecond;
+		if (MoneyPlantUpgrade == 1)
+		{
+			TempIncome += 2;
+		}
+		if (LuckyCatUpgrade == 1)
+		{
+			TempIncome *= 2;
+		}
+		TotalIncome+=TempIncome;
+	}
+	if (MinigameBuffs > 0)
+	{
+		TotalIncome += TotalIncome * (MinigameBuffs * 0.01);
+	}
+	if (PrestigeLvl == 1)
+	{
+		TotalIncome *= 1.5;
+	}
+	else if (PrestigeLvl == 2)
+	{
+		TotalIncome *= 1.75;
+	}
+	else if (PrestigeLvl == 3)
+	{
+		TotalIncome *= 2;
+	}
+	TotalIncomePerSecond = TotalIncome;
+}
+
+void EquipmentManager::UpdateMoney(float dt)
+{
+	Money += TotalIncomePerSecond*dt;
+}
+
+std::string EquipmentManager::ConvertMoneyToSuitableAmounts(void) //Return Edited String
+{
+	float Temp = Money;
+	if (Money >= 1000000)
+	{
+		Temp = Temp * 0.000001;
+	}
+	else if (Money >= 1000)
+	{
+		Temp = Temp * 0.001;
+	}
+	std::string Temporary = std::to_string(Temp);
+	int Stoppos = -1;
+	for (unsigned int i = 0; i < Temporary.length(); ++i)
+	{
+		if (Temporary[i] == '.')
+		{
+			Stoppos = i;
+		}
+	}
+	Temporary += "0000";
+	if (Stoppos > 0)
+	{
+		Temporary = Temporary.substr(0, Stoppos + 3);
+	}
+	if (Money >= 1000000)
+	{
+		Temporary += 'mil';
+	}
+	else if (Money >= 1000)
+	{
+		Temporary += 'k';
+	}
+	return Temporary;
+}
+
+int EquipmentManager::UnlockPhone(bool unlock)
+{
+	//Checks if got space for Phone
+	for(int i = 0; i < PrestigeLvl*6;++i)
+	{
+		//Check if Equip Array got empty "Work station"
+		if (EquipArray[i] == nullptr && unlock == true)
+		{
+			EquipArray[i] = new Equipment;
+			i = PrestigeLvl * 6;
+		}
+		//if Phonelvl is 0 which means not unlocked
+		if(EquipArray[i]->PhoneLvl == 0)
+		{
+			//If got money show can unlock
+			if (Money >= 50)
+			{
+				//if unlock is true , unlock the phone
+				if (unlock == true)
+				{
+					Money -= 50;
+					EquipArray[i]->PhoneLvl = 1;
+				}
+				CalculateTotalIncome();
+				return 1;
+			}
+			i = PrestigeLvl * 6;
+		}
+	}
+	return 0;
+}
+
+int EquipmentManager::UnlockComputer(bool unlock)
+{
+	//Checks if got space for Computer
+	for (int i = 0; i < PrestigeLvl * 6; ++i)
+	{
+		//Check if Equip Array got empty "Work station"
+		if (EquipArray[i] == nullptr && unlock == true)
+		{
+			EquipArray[i] = new Equipment;
+			i = PrestigeLvl * 6;
+		}
+		//if Computerlvl is 0 which means not unlocked
+		if (EquipArray[i]->ComputerLvl == 0)
+		{
+			//If got money show can unlock
+			if (Money >= 50)
+			{
+				//if unlock is true , unlock the Computer
+				if (unlock == true)
+				{
+					Money -= 50;
+					EquipArray[i]->ComputerLvl = 1;
+				}
+				CalculateTotalIncome();
+				return 1;
+			}
+			i = PrestigeLvl * 6;
+		}
+	} 
+	return 0;
+}
+
+int EquipmentManager::UnlockLuckyCat(bool unlock)
+{
+	//Checks if Lucky Cat Unlocked
+	if (LuckyCatUpgrade == 0)
+	{
+		if (Money >= 10000 && PrestigeLvl >= 2)
+		{
+			if (unlock == true)
+			{
+				Money -= 10000;
+				LuckyCatUpgrade = 1;
+			}
+			CalculateTotalIncome();
+			return 1;
+		}
+	} 
+	return 0;
+}
+
+int EquipmentManager::UnlockMoneyPlant(bool unlock)
+{
+	//Checks if Money Plant Unlocked
+	if (MoneyPlantUpgrade == 0)
+	{
+		if (Money >= 5000 && PrestigeLvl >= 1)
+		{
+			if (unlock == true)
+			{
+				Money -= 5000;
+				MoneyPlantUpgrade = 1;
+			}
+			CalculateTotalIncome();
+			return 1;
+		}
+	} 
+	return 0;
+}
+
+int EquipmentManager::UpgradePhone(bool upgrade, int WorkStation)
+{
+	if (EquipArray[WorkStation]->PhoneLvl == 1) //Can Upgrade to 1
+	{
+		if (Money >= 250)
+		{
+			if (upgrade == true)
+			{
+				Money -= 250;
+				EquipArray[WorkStation]->PhoneLvl = 2;
+			}
+			CalculateTotalIncome();
+			return 1;
+		}
+	}
+	if (EquipArray[WorkStation]->PhoneLvl == 2) //Can Upgrade to 2
+	{
+		if (Money >= 1250)
+		{
+			if (upgrade == true)
+			{
+				Money -= 1250;
+				EquipArray[WorkStation]->PhoneLvl = 3;
+			}
+			CalculateTotalIncome();
+			return 1;
+		}
+	}
+	if (EquipArray[WorkStation]->PhoneLvl == 3) //Maxed Cannot upgrade
+	{
+		return 2;
+	}
+	return 0;
+}
+
+int EquipmentManager::UpgradeComputer(bool upgrade, int WorkStation)
+{
+	if (EquipArray[WorkStation]->ComputerLvl == 1) //Can Upgrade to 1
+	{
+		if (Money >= 500)
+		{
+			if (upgrade == true)
+			{
+				Money -= 500;
+				EquipArray[WorkStation]->ComputerLvl = 2;
+			}
+			CalculateTotalIncome();
+			return 1;
+		}
+	}
+	if (EquipArray[WorkStation]->ComputerLvl == 2) //Can Upgrade to 2
+	{
+		if (Money >= 5000)
+		{
+			if (upgrade == true)
+			{
+				Money -= 5000;
+				EquipArray[WorkStation]->ComputerLvl = 3;
+			}
+			CalculateTotalIncome();
+			return 1;
+		}
+	}
+	if (EquipArray[WorkStation]->ComputerLvl == 3) //Maxed Cannot upgrade
+	{
+		return 2;
+	}
+	return 0;
+}
+
+void EquipmentManager::DeleteEquipArray(void)
+{
+	for (int i = 0; i < 18; ++i)
+	{
+		Equipment* Temp = EquipArray[i];
+		delete Temp;
+	}
+}
+
+void EquipmentManager::InitEquipArray(void)
+{
+	for (int i = 0; i < 18; ++i)
+	{
+		EquipArray[i] = nullptr;
+	}
+}
+
